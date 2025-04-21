73a30ef99533ae2f24b36161794524d511d24fa3
Shruti Kulkarni <shrutiikulkarni210399@gmail.com>
2025-04-21 18:04:14 +0530

    Add Vercel Analytics to the project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
     <Router>
+      <Analytics />
       <Routes>
         <Route path="/" element={<Dashboard />} />
       </Routes>
